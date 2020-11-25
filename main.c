f1436a8ad35b26f561aa1f3e80132e8d9b8d0908
Roc <597540267@qq.com>
2020-11-25 11:57:28 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 
 #include "pca9685.h"
 
+void delay(int i) {
+	while (i) {
+		i--;
+	}
+}
+
 int main(void) {
 	PCA9685_Init();
 	while(1){
-		PCA9685_SetPWM(0, 0, (u32)(4096*(0.5/20));
-    delay(80000000);
-		PCA9685_SetPWM(0, 0, (u32)(4096*(1.5/20));
-    delay(80000000);
+		PCA9685_SetPWM(0, 0, (u32)(4096*(0.5/20)); // 0 degree
+		delay(80000000);
+		PCA9685_SetPWM(0, 0, (u32)(4096*(2.5/20)); // 180 degree
+		delay(80000000);
 	}
 }
