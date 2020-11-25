a03bb2d9fbd485e13695f82cb0f15ea31f698f20
Roc <597540267@qq.com>
2020-11-25 12:18:13 +0800

    Update pca9685.h

--- pca9685.h
+++ pca9685.h
@@ -4,6 +4,10 @@
 #include "stm32f10x.h"
 
 void PCA9685_Init();
+
+// channel: 0 ~ 15
+//      on: 0 ~ 4095
+//     off: 0 ~ 4095
 void PCA9685_SetChannelDuty(u8 channel, u32 on, u32 off);
 
 #endif
