c25c2a7582c359d0ceb26198c9b1810ecc8d529e
Roc <597540267@qq.com>
2020-11-25 12:23:19 +0800

    Update main.c

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ void delay(int i) {
 int main(void) {
 	PCA9685_Init();
 	while(1){
-		PCA9685_SetChannelDuty(0, 0, (u32)(4096*(0.5/20)); // 0 degree
+		PCA9685_SetChannelDuty(0, 0, (u32)(4095*(0.5/20)); // 0 degree
 		delay(80000000);
-		PCA9685_SetChannelDuty(0, 0, (u32)(4096*(2.5/20)); // 180 degree
+		PCA9685_SetChannelDuty(0, 0, (u32)(4095*(2.5/20)); // 180 degree
 		delay(80000000);
 	}
 }
